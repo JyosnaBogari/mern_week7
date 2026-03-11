3068d3dec87e3009675e28905cb25f2a4b31b1b4
Bogari Jyosna <jyosnabogari05@gmail.com>
2026-03-11 10:52:28 +0530

    Update Products

--- react-demo-handson/src/components/Products.jsx
+++ react-demo-handson/src/components/Products.jsx
@@ -87,7 +87,7 @@ const products = [
 ]
 
 return(
-    <div>
+    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {
             products.map((msgObj,index)=><Product products={msgObj} key={index} /> ) 
         }
